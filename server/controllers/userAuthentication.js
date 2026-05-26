22abbfdec2aa36dcec10aa2578fdb2cc82b6bb85
Nitin Kumar <nitbit710@gmail.com>
2026-05-26 19:23:28 +0530

    feat:jwt token add in registerUser

--- server/controllers/userAuthentication.js
+++ server/controllers/userAuthentication.js
@@ -1,6 +1,6 @@
 const User=require('../model/user');
 const bcrypt=require('bcrypt');
-
+const jwt=require('jsonwebtoken');
 const register = async (req, res) => {
     const { name, email, password } = req.body;
     if (!name || !email || !password) {
@@ -12,7 +12,17 @@ const register = async (req, res) => {
         res.json("User already exists");
     }
     const user = await User.create(req.body);
+   console.log(user);
+  const token=  jwt.sign({
+     id:user._id,
+     email:user.email
+     }, process.env.JWT_secret, { expiresIn: '1h' });
+     res.cookie('token', token,{
+   maxAge: 60 * 60 * 1000
+});
     res.json("User registered successfully");
+    
+    console.log(token);
 }
 
 module.exports = register;
