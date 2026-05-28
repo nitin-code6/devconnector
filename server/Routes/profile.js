16fc35bf8495dbb5f9e66d446d1b9e0fe1a67506
Nitin Kumar <nitbit710@gmail.com>
2026-05-28 11:39:45 +0530

    feat: add getMyProfile api

--- server/Routes/profile.js
+++ server/Routes/profile.js
@@ -3,8 +3,8 @@ const express = require('express');
 const router = express.Router();
 const userMiddleware=require('../Middleware/UserMiddleware')
 
-const {CreateProfile}=require('../controllers/profileAuthentication');
+const {CreateProfile,getMyProfile}=require('../controllers/profileAuthentication');
 console.log(CreateProfile);
 router.post('/create',userMiddleware,CreateProfile);
-
+router.get('/me',userMiddleware,getMyProfile);
 module.exports = router;
--- server/controllers/profileAuthentication.js
+++ server/controllers/profileAuthentication.js
@@ -36,5 +36,33 @@ const profile = await Profile.create({
 });
 res.status(201).json(profile);
 };
+const getMyProfile = async (req, res) => {
 
-module.exports={CreateProfile};
+   try {
+
+      const user = req.result;
+
+      const profile = await Profile.findOne({
+         user: user._id
+      });
+
+      if (!profile) {
+
+         return res.status(404).json({
+            message: 'Profile not found'
+         });
+
+      }
+
+      res.status(200).json(profile);
+
+   } catch (error) {
+
+      res.status(500).json({
+         message: error.message
+      });
+
+   }
+
+};
+module.exports={CreateProfile,getMyProfile};
