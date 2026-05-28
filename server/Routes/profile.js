6fd71c841ce2a240a9b23196ae2f3472b2931002
Nitin Kumar <nitbit710@gmail.com>
2026-05-28 12:35:39 +0530

    feat:add getProfile Api

--- server/Routes/profile.js
+++ server/Routes/profile.js
@@ -3,8 +3,9 @@ const express = require('express');
 const router = express.Router();
 const userMiddleware=require('../Middleware/UserMiddleware')
 
-const {CreateProfile,getMyProfile}=require('../controllers/profileAuthentication');
-console.log(CreateProfile);
+const {CreateProfile,getMyProfile,getProfile}=require('../controllers/profileAuthentication');
+console.log(getProfile);
 router.post('/create',userMiddleware,CreateProfile);
 router.get('/me',userMiddleware,getMyProfile);
+router.get('/:username',userMiddleware,getProfile);
 module.exports = router;
--- server/controllers/profileAuthentication.js
+++ server/controllers/profileAuthentication.js
@@ -41,9 +41,9 @@ const getMyProfile = async (req, res) => {
    try {
 
       const user = req.result;
-
+      console.log(user);
       const profile = await Profile.findOne({
-         user: user._id
+        user:user._id
       });
 
       if (!profile) {
@@ -65,4 +65,26 @@ const getMyProfile = async (req, res) => {
    }
 
 };
-module.exports={CreateProfile,getMyProfile};
+
+const getProfile=async(req,res)=>{
+    try{
+        const profile=await Profile.findOne({
+            username:req.params.username
+        })
+           if (!profile) {
+
+         return res.status(404).json({
+            message: 'Profile not found'
+         });
+
+      }
+
+      res.status(200).json(profile);
+
+    } catch(error){
+        res.status(500).json({
+            message:error.message
+        })
+    }
+}
+module.exports={CreateProfile,getMyProfile,getProfile};
